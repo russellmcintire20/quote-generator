83587ba0c0105ac6b761470ee66e3c65af901e42
Russell McIntire <russell.mcintire20@gmail.com>
2023-02-27 23:23:13 -0600

    add js const/remove placeholder

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ let apiQuotes = [];
 function newQuote() {
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
